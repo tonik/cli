0162fb51bcd39939f5ecb5f9293a14880aeeb7d9
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-03-03 13:27:08 +0100

    Adds test for CLI::run() method

--- tests/CLI/CLITest.php
+++ tests/CLI/CLITest.php
@@ -65,7 +65,7 @@ class CLITest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function test_asking_for_a_confrimation()
+    public function test_asking_for_a_confrimation_with_true_input()
     {
         $input = Mockery::mock('League\CLImate\TerminalObject\Dynamic\Input');
 
@@ -73,8 +73,74 @@ class CLITest extends PHPUnit_Framework_TestCase
 
         $input->shouldReceive('confirmed')->once()->withNoArgs()->andReturn(true);
         $this->assertTrue($this->cli->askForConfirmation());
+    }
+
+    /**
+     * @test
+     */
+    public function test_asking_for_a_confrimation_with_false_input()
+    {
+        $input = Mockery::mock('League\CLImate\TerminalObject\Dynamic\Input');
+
+        $this->climate->shouldReceive('confirm')->once()->with('Continue?')->andReturn($input);
 
         $input->shouldReceive('confirmed')->once()->withNoArgs()->andReturn(false);
         $this->assertFalse($this->cli->askForConfirmation());
+    }
+
+    /**
+     * @test
+     */
+    public function test_execution_run_with_help_argument()
+    {
+        $shake = Mockery::mock('Tonik\CLI\Command\Shake');
+
+        $this->test_drawing_a_banner();
+
+        $this->arguments->shouldReceive('parse')->once()->withNoArgs();
+        $this->arguments->shouldReceive('defined')->once()->with('help')->andReturn(true);
+        $this->climate->shouldReceive('usage')->once()->withNoArgs();
+
+        $this->cli->run($shake);
+    }
+
+    /**
+     * @test
+     */
+    public function test_proper_execution_run()
+    {
+        $shake = Mockery::mock('Tonik\CLI\Command\Shake');
+
+        $this->arguments->shouldReceive('parse')->once()->withNoArgs();
+        $this->arguments->shouldReceive('defined')->once()->with('help')->andReturn(false);
+
+        $this->test_drawing_a_banner();
+        $this->test_asking_a_questions();
+        $this->test_asking_for_a_confrimation_with_true_input();
+
+        $shake->shouldReceive('rename')->once();
+        $this->climate->shouldReceive('out')->with('<success>Done.</success>');
+
+        $this->cli->run($shake);
+    }
+
+    /**
+     * @test
+     */
+    public function test_abored_execution_run()
+    {
+        $shake = Mockery::mock('Tonik\CLI\Command\Shake');
+
+        $this->arguments->shouldReceive('parse')->once()->withNoArgs();
+        $this->arguments->shouldReceive('defined')->once()->with('help')->andReturn(false);
+
+        $this->test_drawing_a_banner();
+        $this->test_asking_a_questions();
+        $this->test_asking_for_a_confrimation_with_false_input();
+
+        $shake->shouldReceive('rename')->never();
+        $this->climate->shouldReceive('whisper')->with('Shaking abored.');
+
+        $this->cli->run($shake);
     }
 }
